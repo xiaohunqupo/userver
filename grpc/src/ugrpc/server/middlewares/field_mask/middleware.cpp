723fa6845dafb12ed637cf93523fdd7c4a0e5acb
abramov-alex <abramov-alex@yandex-team.com>
2024-10-28 18:11:15 +0300

    feat userver: encode the field mask in the metadata
    Tests: CI
    commit_hash:358896c2f9fb06282d8b83d2aaff74a54333b753

--- grpc/src/ugrpc/server/middlewares/field_mask/middleware.cpp
+++ grpc/src/ugrpc/server/middlewares/field_mask/middleware.cpp
@@ -4,39 +4,24 @@
 #include <utility>
 
 #include <google/protobuf/field_mask.pb.h>
-#include <google/protobuf/stubs/common.h>
 #include <google/protobuf/util/field_mask_util.h>
 #include <grpcpp/support/status.h>
 
 #include <userver/logging/log.hpp>
 #include <userver/ugrpc/field_mask.hpp>
 #include <userver/ugrpc/server/metadata_utils.hpp>
-#include <userver/ugrpc/server/middlewares/field_mask_bin/component.hpp>
+#include <userver/ugrpc/server/middlewares/field_mask/component.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
-namespace ugrpc::server::middlewares::field_mask_bin {
+namespace ugrpc::server::middlewares::field_mask {
 
 namespace {
 
-template <typename T>
-FieldMask ConstructFieldMask(T&& metadata) {
-    google::protobuf::FieldMask google_field_mask;
-    for (const std::string_view field_mask_str : metadata) {
-        google::protobuf::FieldMask one_field_mask;
-
-#if GOOGLE_PROTOBUF_VERSION >= 3022000
-        bool success = one_field_mask.ParseFromString(field_mask_str);
-#else
-        bool success = one_field_mask.ParseFromString(std::string(field_mask_str));
-#endif
-        if (success) {
-            google::protobuf::util::FieldMaskUtil::Union(google_field_mask, one_field_mask, &google_field_mask);
-        } else {
-            LOG_WARNING() << "Failed to construct field mask. Falling back to empty.";
-        }
-    }
-    return FieldMask(google_field_mask);
+FieldMask ConstructFieldMask(ugrpc::server::CallAnyBase& call, std::string_view field_name) {
+    const auto& metadata = GetRepeatedMetadata(call, field_name);
+    if (metadata.empty()) return FieldMask();
+    return FieldMask(*metadata.begin(), FieldMask::Encoding::kWebSafeBase64);
 }
 
 }  // namespace
@@ -46,7 +31,7 @@ Middleware::Middleware(std::string metadata_field_name) : metadata_field_name_(s
 void Middleware::Handle(ugrpc::server::MiddlewareCallContext& context) const {
     try {
         context.GetCall().GetStorageContext().Set(
-            kFieldMaskStorageDataTag, ConstructFieldMask(GetRepeatedMetadata(context.GetCall(), metadata_field_name_))
+            kFieldMaskStorageDataTag, ConstructFieldMask(context.GetCall(), metadata_field_name_)
         );
         context.Next();
     } catch (const FieldMask::BadPathError& e) {
@@ -68,6 +53,6 @@ void Middleware::CallResponseHook(
     }
 }
 
-}  // namespace ugrpc::server::middlewares::field_mask_bin
+}  // namespace ugrpc::server::middlewares::field_mask
 
 USERVER_NAMESPACE_END
