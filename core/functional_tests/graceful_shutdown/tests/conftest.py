06fb42988830e9946d04f8673928f1a7bd45f5b3
antonyzhilin <antonyzhilin@userver.tech>
2024-11-18 19:52:25 +0300

    cc tests: make graceful_shutdown_interval visible commit_hash:645dd2a72bf78789452fc727f0117eaf7489440e

--- core/functional_tests/graceful_shutdown/tests/conftest.py
+++ core/functional_tests/graceful_shutdown/tests/conftest.py
@@ -8,6 +8,8 @@ pytest_plugins = ['pytest_userver.plugins.core']
 def _userver_config_testsuite(userver_config_testsuite):
     def patch_config(config, config_vars) -> None:
         userver_config_testsuite(config, config_vars)
+        # Restore the option after it's deleted by the base fixture.
+        # Don't do this in your testsuite tests! For userver tests only.
         config['components_manager']['graceful_shutdown_interval'] = '3s'
 
     return patch_config
