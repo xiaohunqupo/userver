37ea298f18c7b4947f5caddfacfe1a69561b58d3
antonyzhilin <antonyzhilin@userver.tech>
2024-11-20 11:14:33 +0300

    feat cmake: allow spaces in source dir commit_hash:9700212712246f510899219eb67e66281f488688

--- scripts/grpc/protoc_usrv_plugin.sh
+++ scripts/grpc/protoc_usrv_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GRPC_SCRIPTS_PATH="$(dirname $(realpath $0))"
+GRPC_SCRIPTS_PATH="$(dirname "$(realpath "$0")")"
 GEN_USERVER_WRAPPERS="${GRPC_SCRIPTS_PATH}/generator.py"
 
 USERVER_PYTHON="${USERVER_GRPC_PYTHON_BINARY:-python3}"
