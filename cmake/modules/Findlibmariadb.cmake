d99d2581a9ce1d9eaddd0f1a31b63ad3b90bd4dc
segoon <segoon@userver.tech>
2024-11-21 09:23:41 +0300

    feat mysql: build on alpine/macos commit_hash:e3ab4f4503f3c785dd7251efa5947c2e429dc3b8

--- cmake/modules/Findlibmariadb.cmake
+++ cmake/modules/Findlibmariadb.cmake
@@ -1,3 +1,10 @@
+option(USERVER_MYSQL_OLD_INCLUDE "If yes, uses old mysql/mysql.h instead of mariadb/mysql.h" OFF)
+if(USERVER_MYSQL_OLD_INCLUDE)
+  set(MYSQL_H mysql/mysql.h)
+else()
+  set(MYSQL_H mariadb/mysql.h)
+endif()
+
 _userver_module_begin(
     NAME libmariadb
     VERSION 3.0.3
@@ -5,7 +12,7 @@ _userver_module_begin(
 )
 
 _userver_module_find_include(
-    NAMES mariadb/mysql.h
+    NAMES ${MYSQL_H}
 )
 
 _userver_module_find_library(
