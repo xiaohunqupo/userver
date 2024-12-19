f4b9296ee631a7c23204502b081dd71b8729c88d
antonyzhilin <antonyzhilin@userver.tech>
2024-12-19 10:55:18 +0300

    fix cmake: pin ydb-cpp-sdk version (take 2) commit_hash:e65c1c1e65b07d6a97bac12606e2e483d21cee56

--- cmake/SetupYdbCppSDK.cmake
+++ cmake/SetupYdbCppSDK.cmake
@@ -32,7 +32,7 @@ endif()
 
 CPMAddPackage(
   NAME ydb-cpp-sdk
-  GIT_TAG 22f7e87b7f7263058f37cc9e81cb1af1d70b7589
+  GIT_TAG efddedf40b203e87fe295098c1b4e6eefb3168ed
   GITHUB_REPOSITORY ydb-platform/ydb-cpp-sdk
   OPTIONS
   "Brotli_VERSION ${Brotli_VERSION}"
